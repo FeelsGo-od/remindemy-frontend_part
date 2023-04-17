36cd8470727360d2191bdd37b12683788808b6c8
nocomac <andrew.sokolowsky@gmail.com>
2023-04-18 00:13:59 +0200

    show image when add topic in AddTopicById form

--- src/features/users/AddTopicById.js
+++ src/features/users/AddTopicById.js
@@ -1,50 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 
 import { addUsersTopic } from "./usersSlice";
 
 export default function AddTopicById ({id}) {
     const [text, setText] = useState('')
-    const [fileList, setFileList] = useState(null)
+    const [imageList, setImageList] = useState([])
+    const [imageURLs, setImageURLs] = useState([])
     const [link, setLink] = useState('')
+
+    useEffect(() => {
+        if(imageList.length < 1) return;
+        const newImageUrls = []
+        imageList.forEach(image => newImageUrls.push(URL.createObjectURL(image)))
+        setImageURLs(newImageUrls)
+    }, [imageList])
 
     const dispatch = useDispatch()
 
-    const handleFileChange = (e) => {
-        setFileList(e.target.files)
+    const handleImagesChange = (e) => {
+        if(e.target.files) {
+            setImageList([...e.target.files])
+        }
     }
-
-    const files = fileList ? [...fileList] : [];
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        // if (!fileList) return;
+        if (!imageList) return;
 
-        const imagesData = new FormData()
-        files.forEach((file, i) => {
-            imagesData.append(`file-${i}`, file, file.name)
-        })
+        const imagesData = 'add here links to images from google cloud'
 
-        dispatch(addUsersTopic({id, text, link}))
+        dispatch(addUsersTopic({id, imagesData, text, link}))
     }
 
     return (
         <>
-            <form onSubmit={handleSubmit} className="form mg-none">
+            <form onSubmit={handleSubmit} encType="multipart/form-data" className="form mg-none">
                 <div className="form-block form-input">
                     <label htmlFor="text">Enter the text u want to remember: </label>
                     <textarea onChange={(e) => setText(e.target.value)} value={text} name="text" rows="15" cols="53"></textarea>
                 </div>
                 <div className="form-block form-input">
                     <label htmlFor="images">Add images u want to study later: </label>
-                    <input type="file" onChange={handleFileChange} accept="image/*" multiple name="images" id="images" />
-                    <ul>
-                        {/* {fileList.map((file) => (
-                                <p>{file}</p>
-                            )
-                        )} */}
-                    </ul>
+                    <input type="file" onChange={handleImagesChange} accept="image/*" multiple name="images" id="images" />
+                    <div className="form-uploaded-images">
+                        { imageURLs.map(imageSrc => <img src={imageSrc} className="form-uploaded-img" />) }
+                    </div>
                 </div>
                 <div className="form-block form-input">
                     <label htmlFor="link">Add the link to repeat it later: </label>
